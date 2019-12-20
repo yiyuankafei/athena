404f24fa78c50ab44af79fd8150e574173523bc4
yiyuankafei <124633367@qq.com>
2019-12-20 14:47:45 +0800

    配置文件加密

---
+++ athena-cipher/athena-cipher-jasypt/src/main/java/athena/cipher/jasypt/application/test/DecodeTest.java
@@ -0,0 +1,26 @@
+package athena.cipher.jasypt.application.test;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DecodeTest {
+ 
+    @Autowired
+    private StringEncryptor stringEncryptor;
+ 
+    @Test
+    public void test(){
+ 
+        String encrypt = stringEncryptor.encrypt("root");
+        System.out.println(encrypt);
+        System.out.println(stringEncryptor.decrypt(encrypt));
+ 
+    }
+ 
+}
