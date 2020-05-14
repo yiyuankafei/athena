9ee3dcc601d2640b324538e9d4ab4b2fe612345e
yiyuankafei <124633367@qq.com>
2020-05-14 15:19:05 +0800

    注册beanDefinition

--- athena-spring/athena-spring-circul/src/main/java/com/test/Test.java
+++ athena-spring/athena-spring-circul/src/main/java/com/test/Test.java
@@ -16,20 +16,29 @@ public class Test {
 		BeanDefinitionBuilder genericBeanDefinition = BeanDefinitionBuilder.genericBeanDefinition(D.class);
 		genericBeanDefinition.addPropertyValue("name", "wade");
 		genericBeanDefinition.addPropertyValue("age", 16);
-		ct.registerBeanDefinition("d", genericBeanDefinition.getBeanDefinition());
 		ct.register(AppConfig.class);
 		//ct.setAllowCircularReferences(false);
 		ct.refresh();
 		
-		Object bean = ct.getBean("d");
-		D d = (D)bean;
+		try {
+			Object bean = ct.getBean("d");
+			D d = (D)bean;
+			System.out.println(d.getName());
+			System.out.println(d.getAge());
+		} catch (Exception e) {
+			System.out.println("no d in container");
+		}
+		
+		
+		ct.registerBeanDefinition("d", genericBeanDefinition.getBeanDefinition());
 
 		D d2 = (D)ct.getBean("d");
+		System.out.println(d2.getName());
+		System.out.println(d2.getAge());
 		D d3 = (D)ct.getBean("d");
-		System.out.println(d.getName());
-		System.out.println(d.getAge());
+		
 		System.out.println("====");
-		System.out.println(d == d2);
+		//System.out.println(d == d2);
 		System.out.println(d2 == d3);
 		
 	}
