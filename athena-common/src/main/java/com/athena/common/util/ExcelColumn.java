e1b82080442a20b02018d42d2a23eb8a738d0bb6
yiyuankafei <124633367@qq.com>
2020-07-02 18:54:43 +0800

    excel工具

---
+++ athena-common/src/main/java/com/athena/common/util/ExcelColumn.java
@@ -0,0 +1,32 @@
+package com.athena.common.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author shuhujie
+ * @Date 2020/5/29 13:43
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ExcelColumn {
+    /**
+     * Excel标题
+     *
+     * @return
+     * @author Lynch
+     */
+    String value() default "";
+
+    /**
+     * Excel从左往右排列位置
+     *
+     * @return
+     * @author Lynch
+     */
+    int col() default 0;
+}
