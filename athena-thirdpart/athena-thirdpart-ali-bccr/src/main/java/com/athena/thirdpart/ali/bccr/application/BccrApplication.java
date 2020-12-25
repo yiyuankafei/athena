53fa740aed236f6c47d82298b0484d9a8db9ba9e
yiyuankafei <124633367@qq.com>
2020-12-25 10:13:26 +0800

    阿里取证

---
+++ athena-thirdpart/athena-thirdpart-ali-bccr/src/main/java/com/athena/thirdpart/ali/bccr/application/BccrApplication.java
@@ -0,0 +1,14 @@
+package com.athena.thirdpart.ali.bccr.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BccrApplication {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(BccrApplication.class, args);
+    }
+
+}
