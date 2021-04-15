8f3d0df2060ea48cc70f8a42ea0b6b51b3b3de72
yiyuankafei <124633367@qq.com>
2021-04-15 13:48:36 +0800

    IP工具

---
+++ athena-common/src/main/java/com/athena/common/util/IpUtil.java
@@ -0,0 +1,30 @@
+package com.athena.common.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IpUtil {
+
+	public static String getIpAddress(HttpServletRequest request) {
+        String ip = "";
+        String ipAddresses = request.getHeader("X-Forwarded-For");
+        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            ipAddresses = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            ipAddresses = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            ipAddresses = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            ipAddresses = request.getHeader("X-Real-IP");
+        }
+        if (ipAddresses != null && ipAddresses.length() != 0) {
+            ip = ipAddresses.split(",")[0];
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
