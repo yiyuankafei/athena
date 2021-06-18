37f1ffc2941bba55dec0919ba7701b063029d15b
yiyuankafei <124633367@qq.com>
2021-06-18 11:50:01 +0800

    excel简单读取

---
+++ athena-common/src/main/java/com/athena/common/util/ReadExcelUtil.java
@@ -0,0 +1,37 @@
+package com.athena.common.util;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+public class ReadExcelUtil {
+
+    public static void main(String[] args) throws Exception {
+
+        String filePath = "C:\\Users\\Administrator\\Desktop\\0617.xlsx";
+        FileInputStream is = new FileInputStream(filePath);
+        Workbook workbook = null;
+        if (filePath.endsWith("xlsx")) {
+            workbook = new XSSFWorkbook(is);
+        } else if (filePath.endsWith("xls")) {
+            workbook = new HSSFWorkbook(is);
+        }
+        Sheet sheet = workbook.getSheetAt(0);
+        System.out.println(sheet.getLastRowNum());
+        for (int i = 1; i < sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            double s3 = row.getCell(0).getNumericCellValue();
+            String s5 = row.getCell(4).getStringCellValue();
+
+            System.out.println(s3);
+            System.out.println(s5);
+        }
+    }
+
+}
